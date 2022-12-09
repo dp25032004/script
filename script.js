c16dd0ec1f0ac687181b250887c8047bc84b2687
dp25032004 <115575061+dp25032004@users.noreply.github.com>
2022-12-09 09:53:33 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let name = "Débora";
+console.log(name)
+
+let age = 18;
+console.log(age)
+
+console.log(7 * age)
+
+let friends = ["Nana", "Leticia", "Luana"];
+let numbers = [1, 2, 3];
+console.log(friends[0]);
+
+let person = {
+    name: "Débora",
+    age: 18,
+    isStudent: true
+  };
+  console.log(person.name);
+
+let students = [
+    {
+      name: "Nana",
+      age: 24,
+      isStudent: true
+    },
+    {
+      name: "Leticia",
+      age: 19,
+      isStudent: true
+    },
+    {
+      name: "Luana",
+      age: 18,
+      isStudent: false
+    }
+  ];
+  console.log(students[0].name);
